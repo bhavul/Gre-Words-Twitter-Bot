33da2ee7c88b6ba6c4eb8c622f21c4c02446cec7
Bhavul <bhavul93@gmail.com>
2019-04-03 04:22:38 +0530

    testing by adding frequent cron

--- clock.py
+++ clock.py
@@ -3,6 +3,6 @@ from AwesomeGreBot import tweetANewWord
 
 sched = BlockingScheduler()
 
-sched.add_job(tweetANewWord, 'cron', hour='9-22', minute='10,30,50')
+sched.add_job(tweetANewWord, 'cron', hour='1-23', minute='10,25,30,50,55')
 
 sched.start()
