e65cdcf119ed2ce27340fd20a6d23e2a17d943d4
Bhavul <bhavul93@gmail.com>
2019-04-03 05:02:16 +0530

    Fix misspell; Change cron to once in 30 mins

--- clock.py
+++ clock.py
@@ -3,7 +3,7 @@ from AwesomeGreBot import tweetANewWord
 
 sched = BlockingScheduler()
 
-sched.add_job(tweetANewWord, 'cron', hour='1-23', minute='0,10,20,30,40,50')
+sched.add_job(tweetANewWord, 'cron', hour='1-23', minute='0,30')
 
-print "Starting scheuler!"
+print "Starting scheduler!"
 sched.start()
