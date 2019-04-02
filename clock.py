5e620ad336d2591f477ba2c8ad12b6166391ef33
Bhavul <bhavul93@gmail.com>
2019-04-03 04:50:59 +0530

    cron every 10mins

--- clock.py
+++ clock.py
@@ -3,7 +3,7 @@ from AwesomeGreBot import tweetANewWord
 
 sched = BlockingScheduler()
 
-sched.add_job(tweetANewWord, 'cron', hour='1-23', minute='10,25,30,50,55')
+sched.add_job(tweetANewWord, 'cron', hour='1-23', minute='0,10,20,30,40,50')
 
 print "Starting scheuler!"
 sched.start()
